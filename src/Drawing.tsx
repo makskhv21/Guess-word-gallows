3423c25f321d7d8e9f1b442eae29ecbc7a62e6ca
repo <khvits21@gmail.com>
2024-07-19 12:54:03 +0300

    style: Human part drawn

--- src/Drawing.tsx
+++ src/Drawing.tsx
@@ -1,28 +1,28 @@
 import styled from "styled-components";
 
-const DrawingContainerStyle = styled.div`
+const DrawingContainer = styled.div`
     position: relative;
 `;
 
-const DrawingBaseStyle = styled.div`
+const DrawingBase = styled.div`
     height: 10px;
     width: 250px;
     background: black;
 `;
 
-const DrawingCenterStyle = styled.div`
+const DrawingCenter = styled.div`
     height: 400px;
     width: 10px;
     background: black;
     margin-left: 120px;
 `
-const DrawingArmStyle = styled.div`
+const DrawingArm = styled.div`
     height: 10px;
     width: 200px;
     background: black;
     margin-left: 120px;
 `
-const DrawingHookStyle = styled.div`
+const DrawingHook = styled.div`
     height: 50px;
     width: 10px;
     background: black;
@@ -30,15 +30,80 @@ const DrawingHookStyle = styled.div`
     top: 0;
     right: 0;
 `
+const Head = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    border: 10px solid black;
+    position: absolute;
+    top: 50px;
+    right: -30px;
+`
+const Body = styled.div`
+    width: 10px;
+    height: 100px;
+    background: black;
+    position: absolute;
+    top: 120px;
+    right: 0;
+`
+
+const LeftArm = styled.div`
+    width: 100px;
+    height: 10px;
+    background: black;
+    position: absolute;
+    top: 150px;
+    right: 10px;
+    rotate: 30deg;
+    transform-origin: right bottom
+`
+
+const RightArm = styled.div`
+    width: 100px;
+    height: 10px;
+    background: black;
+    position: absolute;
+    top: 150px;
+    right: -100px;
+    rotate: -30deg;
+    transform-origin: left bottom;
+`
+const LeftLeg = styled.div`
+    width: 100px;
+    height: 10px;
+    background: black;
+    position: absolute;
+    top: 210px;
+    right: 0px;
+    rotate: -60deg;
+    transform-origin: right bottom;
+`
+const RightLeg = styled.div`
+    width: 100px;
+    height: 10px;
+    background: black;
+    position: absolute;
+    top: 210px;
+    right: -90px;
+    rotate: 60deg;
+    transform-origin: left bottom;
+`
 
 const Drawing = () => {
     return(
-    <DrawingContainerStyle>
-        <DrawingHookStyle />
-        <DrawingArmStyle />
-        <DrawingCenterStyle />
-        <DrawingBaseStyle />
-    </DrawingContainerStyle>
+        <DrawingContainer>
+            <DrawingHook />
+            <Head />
+            <Body />
+            <LeftArm />
+            <RightArm />
+            <LeftLeg />
+            <RightLeg />
+            <DrawingArm />
+            <DrawingCenter />
+            <DrawingBase />
+        </DrawingContainer>
     )
 };
 
